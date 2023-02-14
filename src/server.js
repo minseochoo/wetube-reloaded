dc5fda233b633c521c03cc067161c7c262108d1c
minseochoo <cloudiao3o@gmail.com>
2023-02-14 23:36:23 +0900

    ~5.10

--- src/server.js
+++ src/server.js
@@ -8,8 +8,10 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
@@ -17,6 +19,6 @@ app.use("/users", userRouter);
 app.use(logger);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`⭐️Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
